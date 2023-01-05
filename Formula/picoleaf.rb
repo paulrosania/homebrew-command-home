4b0d64243d73582d43b3b85c51378fd1c90ff1e3
Paul Rosania <paul.rosania@gmail.com>
2023-01-04 16:01:11 -0800

    match style guide

--- Formula/picoleaf.rb
+++ Formula/picoleaf.rb
@@ -1,5 +1,5 @@
 class Picoleaf < Formula
-  desc "A tiny CLI tool for controlling Nanoleaf"
+  desc "Tiny CLI tool for controlling Nanoleaf"
   homepage "https://github.com/paulrosania/picoleaf"
   url "https://github.com/paulrosania/picoleaf/archive/refs/tags/v1.2.0.tar.gz"
   sha256 "31e298da324c3c71f00322e4aae3bf7ad845813fd5aad5cdf506e15526b02d07"
--- Formula/tron.rb
+++ Formula/tron.rb
@@ -1,5 +1,5 @@
 class Tron < Formula
-  desc "A tiny CLI tool for controlling Lutron Caséta systems"
+  desc "Tiny CLI tool for controlling Lutron Caséta systems"
   homepage "https://github.com/paulrosania/tron"
   url "https://github.com/paulrosania/tron/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "ba900a9aa7723990ef77dfb20137c56d0abdce93aa91696bc4d4f5d75c93f741"
